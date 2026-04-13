18347605765fa7ba3c121158ccaaf4da2e7b09d9
annas2727 <annasicoli@aol.com>
2026-04-13 13:18:16 -0400

    Fixed shooters

--- Assets/Scripts/ThrowingScript.cs
+++ Assets/Scripts/ThrowingScript.cs
@@ -7,15 +7,13 @@ public class ThrowingScript : MonoBehaviour
     [Header("Shooting Settings")]
     public GameObject rockPrefab; 
     float rockImpulse = 30f; 
-    private ShooterBehavior shooter;
-    private float throwDelay = 0f; // to sync with animation
+    public ShooterBehavior shooter;
+    private float throwDelay = 0.5f;
     private float shotTimer = 0f;
-    private float shotInterval = 0.833f; 
     private bool canShoot = true;
     private float repeatTimer = 0f;
-    private float animTimer = 0f;
-    private bool isAnimating = false;
-    
+
+
     private string bulletTag = "P1Bullet";
 
     void Start()
@@ -27,22 +25,16 @@ public class ThrowingScript : MonoBehaviour
         }
     }
 
-
     void Update()
     {
-        repeatTimer += Time.deltaTime;
-
-        if (canShoot && repeatTimer > shotInterval) 
+        if (repeatTimer > 3f && canShoot) 
         {
+            repeatTimer = 0f;
             if (Time.time >= shooter.lastAttackTime + shooter.attackCooldown)
             {
-                shooter.lastAttackTime = Time.time;
                 canShoot = false; 
                 shotTimer = 0f;
-                animTimer = 0f;
-                isAnimating = true;
-                shooter.isAttacking = true;
-                repeatTimer = 0f;
+                shooter.isAttacking = true; //start animation
             }
         }
 
@@ -52,25 +44,17 @@ public class ThrowingScript : MonoBehaviour
             if (shotTimer >= throwDelay)
             {
                 Shoot();
-                canShoot = true;
-            }
-        }
-
-        if (isAnimating)
-        {
-            animTimer += Time.deltaTime;
-            if (animTimer >= shotInterval)
-            {
+                canShoot = true; 
                 shooter.isAttacking = false;
-                isAnimating = false;
+                shooter.lastAttackTime = Time.time;
             }
         }
+        repeatTimer += Time.deltaTime;
     }
 
     void Shoot()
     {
         GameObject therock = (GameObject)Instantiate(rockPrefab, this.transform.position, this.transform.rotation); 
-        therock.tag = bulletTag;
         therock.GetComponent<Rigidbody>().AddForce(this.transform.forward * rockImpulse, ForceMode.Impulse); //adding force to our rock
     }
 }
