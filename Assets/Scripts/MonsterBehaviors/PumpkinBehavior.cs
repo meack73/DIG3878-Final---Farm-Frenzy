7e04b5bc0a40a0d8ac3fa5ad3178247176cf574e
annas2727 <73410623+annas2727@users.noreply.github.com>
2026-04-20 14:34:34 -0400

    pumpkin crack

--- Assets/Scripts/MonsterBehaviors/PumpkinBehavior.cs
+++ Assets/Scripts/MonsterBehaviors/PumpkinBehavior.cs
@@ -3,15 +3,40 @@ using System.Collections;
 
 public class PumpkinBehavior : MonoBehaviour
 {
+    [Header("Stats")]
     public int health = 3;
     public int playerId;
     public Vector3Int spawnTile;
-    
+
+    [Header("Materials")]
+    public Material orange;
+    public Material cracked;
+    public Material veryCracked;
+
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
     public void TakeDamage(int damage)
     {
         health -= damage;
+        UpdateMaterial();
         if (health <= 0)
             Die();
+    }
+
+
+    private void UpdateMaterial()
+    {
+        if (health == 3)
+            rend.material = orange;
+        else if (health == 2)
+            rend.material = cracked;
+        else
+            rend.material = veryCracked;
     }
 
     private void Die()
